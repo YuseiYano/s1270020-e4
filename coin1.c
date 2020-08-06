90ad0ec0d34f00dba7c8dc4935b8a6583b8f35f8
Yusei Yano <yusei.00.0529@gmail.com>
2020-08-06 20:58:57 +0900

    User name added

--- coin1.c
+++ coin1.c
@@ -5,7 +5,13 @@
 int main(){
   int head = 0;
   int tail = 0;
- 
+  char name[20];
+    
+    printf("Who are you?\n> ");
+    scanf("%s",name);
+    printf("Hi %s",name);
+    
+    
   printf("Tossing a coin...\n");
   srand(time(NULL));
   for(int i=0; i<3; i++){
